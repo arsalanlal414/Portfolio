a7b21124d46dbfdf22821cdc28b2016a28cd4ac5
arsalanlal414 <arsalanlal414@gmail.com>
2024-06-27 14:01:27 +0500

    my personal portfolio

---
+++ src/app/components/AchievementsSection.jsx
@@ -0,0 +1,92 @@
+"use client"
+import React, { useState, useEffect } from "react";
+
+const achievementsList = [
+  {
+    metric: "Projects",
+    value: "20",
+    postfix: "+",
+  },
+  {
+    postfix: "+",
+    metric: "Certifications",
+    value: "45",
+  },
+  {
+    metric: "Awards",
+    value: "7",
+  },
+  {
+    postfix: "+",
+    metric: "Years",
+    value: "4",
+  },
+];
+
+const AchievementsSection = () => {
+  const [animatedNumbers, setAnimatedNumbers] = useState([]);
+
+  useEffect(() => {
+    const animations = achievementsList.map((achievement) => ({
+      start: 0,
+      end: parseInt(achievement.value.replace(",", "")),
+      stepSize: 50, 
+      interval: 0,
+    }));
+
+    // Trigger animations
+    animations.forEach((animation, index) => {
+      animateNumbers(animation, index);
+    });
+
+    setAnimatedNumbers(animations);
+
+    // Clean up intervals on unmount
+    return () => {
+      animations.forEach((animation) => {
+        clearInterval(animation.interval);
+      });
+    };
+  }, []);
+
+  const animateNumbers = (animation, index) => {
+    let current = animation.start;
+    const step = animation.stepSize / Math.abs(animation.end - current);
+
+    animation.interval = setInterval(() => {
+      current += step;
+      if (current >= animation.end) {
+        current = animation.end;
+        clearInterval(animation.interval);
+      }
+      setAnimatedNumbers(prevAnimations => {
+        const newAnimations = [...prevAnimations];
+        newAnimations[index].start = current;
+        return newAnimations;
+      });
+    }, 50); // Adjust interval for smoother animation
+  };
+
+  return (
+    <div className="py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+      <div className="sm:border-[#33353F] sm:border rounded-md py-8 px-16 flex flex-col sm:flex-row items-center justify-between">
+        {achievementsList.map((achievement, index) => (
+          <div
+            key={index}
+            className="flex flex-col items-center justify-center mx-4 my-4 sm:my-0"
+          >
+            <h2 className="text-white text-4xl font-bold flex flex-row">
+              <span className="animated-number">
+                {animatedNumbers[index] && Math.abs(Math.round(animatedNumbers[index].start)).toLocaleString()}
+              </span>
+              {achievement.postfix}
+            </h2>
+            <p className="text-[#ADB7BE] text-base">{achievement.metric}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default AchievementsSection;
---
+++ src/app/components/ProjectsSection.jsx
@@ -0,0 +1,128 @@
+"use client";
+import React, { useState, useRef } from "react";
+import ProjectCard from "./ProjectCard";
+import ProjectTag from "./ProjectTag";
+import { motion, useInView } from "framer-motion";
+
+const projectsData = [
+  {
+    id: 1,
+    title: "Java Times Cafe",
+    description: "Ultimate destination for premium coffee, tea, chocolate, and bakery delights worldwide.",
+    image: "/images/projects/javatimescaffe.png",
+    tag: ["All", "Web"],
+    gitUrl: "https://javatimescaffe.com/en",
+    previewUrl: "https://javatimescaffe.com/en",
+  },
+  {
+    id: 2,
+    title: "Admin Dashboard",
+    description: "Admin Dashboard build with React and TypeScript. Helps the Admins to navigate all the stats.",
+    image: "/images/projects/adminDashboard.png",
+    tag: ["All", "Web"],
+    gitUrl: "https://github.com/arsalanlal414/admin-dashboard",
+    previewUrl: "https://stats-dashboard.surge.sh/",
+  },
+  {
+    id: 3,
+    title: "Nexcent Landing Page",
+    description: "Nexcent Landing Page build with React. I have created a responsive User Interface to help the user navigate easily",
+    image: "/images/projects/nexcent_landing_page.png",
+    tag: ["All", "Web"],
+    gitUrl: "https://github.com/arsalanlal414/Nexcent_LandingPage",
+    previewUrl: "http://nexcentt.surge.sh/",
+  },
+  {
+    id: 4,
+    title: "Receipe Master",
+    description: "Receipe Master is a website which helps to see the recipes of different foods. It is built with react have integrated the Food API to navigate more easily.",
+    image: "/images/projects/recipeApp.png",
+    tag: ["All", "Mobile", "Web"],
+    gitUrl: "https://github.com/arsalanlal414/recipe_master",
+    previewUrl: "http://recipemasters.surge.sh",
+  },
+  {
+    id: 5,
+    title: "Chat App",
+    description: "Desktop version of chat app created with react and firebase. also integrated the user authentication.",
+    image: "/images/projects/smooty-chatapp.png",
+    tag: ["All", "Web"],
+    gitUrl: "https://github.com/arsalanlal414/Firebase-Chat-App",
+    previewUrl: "http://smootychatapp.surge.sh/",
+  },
+  {
+    id: 6,
+    title: "React Tabs switch",
+    description: "React Tabs Switch: A simple, customizable React component for smooth tabbed navigation.",
+    image: "/images/projects/Tabs.png",
+    tag: ["All", "Web"],
+    gitUrl: "https://github.com/arsalanlal414/chai-aur-react",
+    previewUrl: "http://tabs-06.surge.sh",
+  },
+];
+
+const ProjectsSection = () => {
+  const [tag, setTag] = useState("All");
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
+  const handleTagChange = (newTag) => {
+    setTag(newTag);
+  };
+
+  const filteredProjects = projectsData.filter((project) =>
+    project.tag.includes(tag)
+  );
+
+  const cardVariants = {
+    initial: { y: 50, opacity: 0 },
+    animate: { y: 0, opacity: 1 },
+  };
+
+  return (
+    <section id="projects">
+      <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
+        My Projects
+      </h2>
+      <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
+        <ProjectTag
+          onClick={handleTagChange}
+          name="All"
+          isSelected={tag === "All"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="Web"
+          isSelected={tag === "Web"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="Mobile"
+          isSelected={tag === "Mobile"}
+        />
+      </div>
+      <ul ref={ref} className="grid md:grid-cols-3 gap-8 md:gap-12">
+        {filteredProjects.map((project, index) => (
+          <motion.li
+            key={index}
+            variants={cardVariants}
+            initial="initial"
+            animate={isInView ? "animate" : "initial"}
+            transition={{ duration: 0.3, delay: index * 0.4 }}
+          >
+            <ProjectCard
+              key={project.id}
+              title={project.title}
+              description={project.description}
+              imgUrl={project.image}
+              gitUrl={project.gitUrl}
+              previewUrl={project.previewUrl}
+            />
+          </motion.li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default ProjectsSection;
